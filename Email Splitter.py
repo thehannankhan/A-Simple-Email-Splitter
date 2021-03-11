e37900c79e9b0e7444fcea918089f5d853d95797
Hannan <64326120+thehannankhan@users.noreply.github.com>
2021-03-11 10:02:06 +0500

    Add files via upload
    Adding main .py file

---
+++ Email Splitter.py
@@ -0,0 +1,12 @@
+# Taking an input Email and using 'strip()' to split
+email = input("Enter Email:").strip()
+
+# Cut before the '@' Symbol
+userName = email[:email.index("@")]
+# Cut after the '@' Symbol
+domainName = email[email.index("@")+1:]
+
+#Using '.format()' for more effecient formatting
+output = "Username: '{}'    Password: '{}'".format(userName,domainName)
+
+print(output)
